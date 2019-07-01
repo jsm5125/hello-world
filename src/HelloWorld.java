342a544d3d1f96d0d18a4655b5845d240bb73f30
jsm51 <jsm51@192.168.219.100>
2019-07-02 01:15:30 +0900

    syso 한줄 추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String []args){
 		System.out.println("Hello World");
 		System.out.println("Hello again");
+		System.out.println("It's me Sungmin");
 	}
 }
